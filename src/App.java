eddb3330b627a82b73cc16fa7b61879b6c456f5e
gykim93 <gykim1993@gmail.com>
2025-06-23 20:31:53 +0900

    1단계 완료

--- src/App.java
+++ src/App.java
@@ -3,12 +3,16 @@ import java.util.Scanner;
 class App {
     void run() {
         System.out.println("== 명언 앱 ==");
-        System.out.print("명령 ) ");
 
-        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print("명령 ) ");
 
-        String cmd = scanner.nextLine();
+            Scanner scanner = new Scanner(System.in);
+            String cmd = scanner.nextLine();
 
-        System.out.printf("입력하신 명령 : %s\n", cmd);
+            if (cmd.equals("종료")) {
+                break;
+            }
+        }
     }
 }
